a3d4e4c6a19e8e0afeff4ffb4ae8dd09ed91bca7
张龙 <foreverelement@163.com>
2017-06-19 21:56:50 +0800

    动态增加奖品功能修改

--- src/cms/views/activities/main.vue
+++ src/cms/views/activities/main.vue
@@ -29,7 +29,7 @@
                 <el-tabs type="border-card">
                   <el-tab-pane>
                     <span slot="label"><i class="el-icon-setting"></i> 基础设置</span>
-                    <el-form :model="activity.base_setting" :rules="base_settingRules" ref="activity.base_setting" label-width="100px">
+                    <el-form :model="activity.base_setting" :rules="base_setting_rules" ref="activity.base_setting" label-width="100px">
                       <el-form-item label="活动名称" porp="activity_name" required>
                         <el-input v-model="activity.base_setting.activity_name"></el-input>
                       </el-form-item>
@@ -52,13 +52,22 @@
                         <el-switch on-text="隐藏" off-text="显示" v-model="activity.base_setting.is_join_num"></el-switch>
                       </el-form-item>
                       <el-form-item label="" v-if="activity.base_setting.is_join_num">
-                        在实际参与人数基础上增加<el-input v-model="activity.base_setting.fictitious_join_num" style="width:50px"></el-input>人(只展示不计入系统)
+                        <el-col :span="16">
+                          <el-input placeholder="请输入内容" v-model="activity.base_setting.fictitious_join_num">
+                            <template slot="prepend">在实际参与人数基础上增加</template>
+                            <template slot="append">人(只展示不计入系统)</template>
+                          </el-input>
+                        </el-col>
                       </el-form-item>
                       <el-form-item label="参与人数限制" prop="is_join_num_limit">
                         <el-switch on-text="限制" off-text="不限" v-model="activity.base_setting.is_join_num_limit"></el-switch>
                       </el-form-item>
                       <el-form-item label="" v-if="activity.base_setting.is_join_num_limit">
-                          <el-input v-model="activity.base_setting.join_num" style="width:50px"></el-input>人参与
+                        <el-col :span="16">
+                          <el-input placeholder="请输入内容" v-model="activity.base_setting.join_num">
+                            <template slot="append">人参与</template>
+                          </el-input>
+                        </el-col>
                       </el-form-item>
                       <el-form-item label="活动说明" prop="activity_des">
                           <el-input type="textarea" :autosize="{ minRows: 4, maxRows: 8}" v-model="activity.base_setting.activity_des"></el-input>
@@ -67,15 +76,109 @@
                   </el-tab-pane>
                   <el-tab-pane>
                     <span slot="label"><i class="el-icon-menu"></i> 派奖方式</span>
-                    222
+                    <el-form :model="activity.lottery_setting" :rules="lottery_setting_rules" ref="activity.lottery_setting" label-width="120px">
+                      <el-form-item label="总抽奖机会" porp="activity.lottery_setting.is_total_lottery">
+                        <el-col :span="6">
+                          <el-radio label="0" v-model="activity.lottery_setting.is_total_lottery">不限</el-radio>
+                          <el-radio label="1" v-model="activity.lottery_setting.is_total_lottery">限制</el-radio>
+                        </el-col>
+                        <el-col :span="12">
+                          <el-input placeholder="请输入内容" v-model="activity.lottery_setting.total_lottery_time" v-if="activity.lottery_setting.is_total_lottery == 1">
+                            <template slot="prepend">每人最多有</template>
+                            <template slot="append">次抽奖机会</template>
+                          </el-input>
+                        </el-col>
+                      </el-form-item>
+                      <el-form-item label="每日抽奖机会">
+                        <el-input placeholder="请输入内容" v-model="activity.lottery_setting.daily_lottery_time">
+                          <template slot="prepend">每人每日有</template>
+                          <template slot="append">次抽奖机会</template>
+                        </el-input>
+                      </el-form-item>
+                      <el-form-item label="每人中奖次数">
+                        <el-input placeholder="请输入内容" v-model="activity.lottery_setting.winning_time">
+                          <template slot="prepend">每人组多可中奖</template>
+                          <template slot="append">次</template>
+                        </el-input>
+                      </el-form-item>
+
+                      <el-form-item label="好友助力" prop="is_join_num_limit">
+                        <el-col :span="12">
+                          <el-radio label="0" v-model="activity.lottery_setting.friend_booster">关闭</el-radio>
+                          <el-radio label="1" v-model="activity.lottery_setting.friend_booster">分享奖励</el-radio>
+                          <el-radio label="2" v-model="activity.lottery_setting.friend_booster">邀请奖励</el-radio>
+                        </el-col>
+                      </el-form-item>
+                      <el-form-item label="" v-if="activity.lottery_setting.friend_booster == '1'">
+                        <el-input placeholder="请输入内容" v-model="activity.lottery_setting.friend_sharing_time">
+                          <template slot="prepend">分享给好友或朋友圈，当天将额外获得</template>
+                          <template slot="append">次抽奖机会</template>
+                        </el-input>
+                        <el-col>
+                          <div class="text-tips">
+                            <p>分享奖励仅当日首次分享有效；</p>
+                            <p>分享提示语仅在抽奖次数为零时提示，次数不为零时分享也会额外增加抽奖机会；</p>
+                            <p>开启有被微信拉黑朋友圈仅自己可见的风险，请慎用！</p>
+                          </div>
+                        </el-col>
+                      </el-form-item>
+                      <el-form-item v-if="activity.lottery_setting.friend_booster == '2'">
+                        <el-input placeholder="请输入内容" v-model="activity.lottery_setting.friend_Invitation_time">
+                          <template slot="prepend">每成功邀请一位好友，当天将额外获得一次抽奖机会，每天最多获得</template>
+                          <template slot="append">次</template>
+                        </el-input>
+                        <el-col>
+                          <div class="text-tips">
+                            <p>好友成功参与后才能获得奖励，邀请同一位好友无效；</p>
+                            <p>分享提示语仅在抽奖次数为零时提示，次数不为零时分享也会额外增加抽奖机会；</p>
+                            <p>开启有被微信拉黑朋友圈仅自己可见的风险，请慎用！</p>
+                          </div>
+                        </el-col>
+                      </el-form-item>
+                    </el-form>
                   </el-tab-pane>
                   <el-tab-pane>
                     <span slot="label"><i class="el-icon-star-on"></i> 奖项设置</span>
-                    333
+                    <el-tabs v-model="edit_awards_tabs_value" type="card" editable @edit="handleTabsEdit">
+                      <el-tab-pane
+                        v-for="(item, index) in activity.award_setting"
+                        :label="item.award_level"
+                        :name="item.name"
+                      >
+                      <el-form :rules="award_setting_rules" ref="activity.award_setting" label-width="120px">
+                        <el-form-item label="奖品等级">
+                          <el-input v-model="item.award_level" disabled></el-input>
+                        </el-form-item>
+                        <el-form-item label="奖品类型">
+                          <el-radio label="0" v-model="item.award_type">流量</el-radio>
+                          <el-radio label="1" v-model="item.award_type">流量券</el-radio>
+                          <el-radio label="2" v-model="item.award_type">实物</el-radio>
+                        </el-form-item>
+                        <el-form-item label="奖品名称">
+                          <el-input v-model="item.award_name"></el-input>
+                        </el-form-item>
+                        <el-form-item label="奖品数量">
+                          <el-input v-model="item.award_num"></el-input>
+                        </el-form-item>
+                        <el-form-item label="中奖概率">
+                          <el-input v-model="item.winning_rate">
+                            <template slot="append">%</template>
+                          </el-input>
+                        </el-form-item>
+                        <el-form-item label="有效期开始时间">
+                          <el-date-picker v-model="item.limit_begin_time" type="datetime" placeholder="选择开始时间" style="width:100%;">
+                         </el-date-picker>
+                        </el-form-item>
+                        <el-form-item label="有效期结束时间">
+                          <el-date-picker v-model="item.limit_end_time" type="datetime" placeholder="选择结束时间" style="width:100%;">
+                         </el-date-picker>
+                        </el-form-item>
+                      </el-form>
+                      </el-tab-pane>
+                    </el-tabs>
                   </el-tab-pane>
                   <el-tab-pane>
                     <span slot="label"><i class="el-icon-share"></i> 分享设置</span>
-                    444
                   </el-tab-pane>
                   <el-tab-pane>
                     <span slot="label"><i class="el-icon-more"></i> 高级设置</span>
@@ -95,7 +198,9 @@
     data() {
       return {
         activeName: '1',
-        item: '张三',
+        edit_awards_tabs_value: '1',
+        tab_awards_name_list: ['奖项一','奖项二','奖项三','奖项四','奖项五','奖项六','奖项七','奖项八'],
+        tabIndex: 1,
         activity: {
           // 基础设置
           base_setting: {
@@ -110,41 +215,26 @@
           },
           // 派奖方式
           lottery_setting: {
-            is_total_lottery: false,      // 是否限制总抽奖机会 boolean
-            total_lottery_time: 0,        // 总抽奖次数
-            daily_lottery_time: 0,        // 每人每日抽奖次数
-            winning_time: 0,              // 每人中奖次数
+            is_total_lottery: '0',        // 是否限制总抽奖机会 boolean
+            total_lottery_time: 6,        // 总抽奖次数
+            daily_lottery_time: 3,        // 每人每日抽奖次数
+            winning_time: 1,              // 每人中奖次数
             // total_winning_rate: 10,       // 总中奖概率 百分比
-            friend_booster: 0,            // 好友助力 0关闭 1分享奖励 2邀请奖励
+            friend_booster: '0',            // 好友助力 0关闭 1分享奖励 2邀请奖励
             friend_sharing_time: 0,       // 分享活动可获得的抽奖次数
             friend_Invitation_time: 0     // 每成功邀请一位好友，当天将额外获得一次抽奖机会，每天最多获得n次
           },
           // 奖项设置
           award_setting: [
             {
-              award_level: '一等奖',       // 奖品等级
-              award_type: 0,              // 奖品类型 0流量 1流量券 2实物   活动发布后类型无法修改
+              title: 'Tab 1',
+              name: '1',
+              content: 'Tab 1 content',
+              award_level: '奖项一',       // 奖品等级
+              award_type: '0',            // 奖品类型 0流量 1流量券 2实物   活动发布后类型无法修改
               award_name: '100M流量',      // 奖品名称                    活动发布后名称无法修改
-              award_num: 0,               // 奖品数量
-              winning_rate: 0,            // 中奖概率
-              limit_begin_time: '',       // 限制有效期开始时间
-              limit_end_time: ''          // 限制有效期结束时间
-            },
-            {
-              award_level: '一等奖',       // 奖品等级
-              award_type: 0,              // 奖品类型 0流量 1流量券 2实物   活动发布后类型无法修改
-              award_name: '100M流量',      // 奖品名称                    活动发布后名称无法修改
-              award_num: 0,               // 奖品数量
-              winning_rate: 0,            // 中奖概率
-              limit_begin_time: '',       // 限制有效期开始时间
-              limit_end_time: ''          // 限制有效期结束时间
-            },
-            {
-              award_level: '一等奖',       // 奖品等级
-              award_type: 0,              // 奖品类型 0流量 1流量券 2实物   活动发布后类型无法修改
-              award_name: '100M流量',      // 奖品名称                    活动发布后名称无法修改
-              award_num: 0,               // 奖品数量
-              winning_rate: 0,            // 中奖概率
+              award_num: 100,             // 奖品数量
+              winning_rate: 10,           // 中奖概率
               limit_begin_time: '',       // 限制有效期开始时间
               limit_end_time: ''          // 限制有效期结束时间
             }
@@ -177,7 +267,19 @@
             }
           }
         },
-        base_settingRules: {
+        base_setting_rules: {
+          activity_name: [
+            { required: true, message: '请输入活动名称', trigger: 'blur' },
+            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+          ],
+        },
+        lottery_setting_rules: {
+          activity_name: [
+            { required: true, message: '请输入活动名称', trigger: 'blur' },
+            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+          ],
+        },
+        award_setting_rules: {
           activity_name: [
             { required: true, message: '请输入活动名称', trigger: 'blur' },
             { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
@@ -194,23 +296,25 @@
       handleClick(tab, event) {
         console.log(tab, event)
       },
-      tabPrev () {
-        let tabLength = $('.el-tabs__item').length
-        if(this.activeName <= tabLength){
-          this.activeName = (parseInt(this.activeName) - 1) + ''
-          if(this.activeName < '1'){
-            this.activeName = tabLength + ''
-          }
-        }
-      },
-      tabNext () {
-        let tabLength = $('.el-tabs__item').length
-        if(this.activeName < tabLength){
-          this.activeName = (parseInt(this.activeName) + 1) + ''
-        } else {
-          this.activeName = '1'
-        }
-      },
+      // 切换选项卡tab
+      // tabPrev () {
+      //   let tabLength = $('.el-tabs__item').length
+      //   if(this.activeName <= tabLength){
+      //     this.activeName = (parseInt(this.activeName) - 1) + ''
+      //     if(this.activeName < '1'){
+      //       this.activeName = tabLength + ''
+      //     }
+      //   }
+      // },
+      // tabNext () {
+      //   let tabLength = $('.el-tabs__item').length
+      //   if(this.activeName < tabLength){
+      //     this.activeName = (parseInt(this.activeName) + 1) + ''
+      //   } else {
+      //     this.activeName = '1'
+      //   }
+      // },
+      // 重载 iframe 页面里的数据
       load (item) {
         console.log(this.$refs)
         const app = this.$refs.iframe.contentWindow.app
@@ -218,6 +322,62 @@
           app.setContent(this.item)
         } else {
           window._item = this.item
+        }
+      },
+      // 修改奖品tabs
+      handleTabsEdit(targetName, action) {
+        let tab_length = this.activity.award_setting.length
+        if (action === 'add') {
+          let tab_name = ''
+          let tab_targetName = ++this.tabIndex + ''
+          for (let i = 0;i<tab_length;i++){
+            tab_name = this.tab_awards_name_list[i+1]
+          }
+          if (tab_length < 8){
+            this.activity.award_setting.push({
+              name: tab_targetName,
+              content: 'New Tab content',
+              award_level: tab_name,
+              award_type: '0',
+              award_name: '100M流量',
+              award_num: 100,
+              winning_rate: 10,
+              limit_begin_time: '',
+              limit_end_time: ''
+            });
+            this.edit_awards_tabs_value = tab_targetName;
+          }else{
+            this.$notify({
+              title: '提示',
+              message: '最多可添加8个奖品',
+              type: 'warning'
+            });
+          }
+        }
+        if (action === 'remove') {
+          let tabs = this.activity.award_setting
+          let activeName = this.edit_awards_tabs_value
+          if(tab_length > 1){
+            if (activeName === targetName) {
+              tabs.forEach((tab, index) => {
+                if (tab.name === targetName) {
+                  let nextTab = tabs[index + 1] || tabs[index - 1];
+                  if (nextTab) {
+                    activeName = nextTab.name;
+                  }
+                }
+              });
+            }
+
+            this.edit_awards_tabs_value = activeName;
+            this.activity.award_setting = tabs.filter(tab => tab.name !== targetName);
+          }else{
+            this.$notify({
+              title: '提示',
+              message: '请最少保留1个奖品',
+              type: 'warning'
+            });
+          }
         }
       }
     }
@@ -257,6 +417,14 @@
       font-size: 0.85rem;
       line-height: 1.05rem;
       background: #eeeeee;
+  }
+}
+.text-tips {
+  padding: 10px 0;
+  color: #8492A6;
+  p{
+    padding:0;
+    margin:0;
   }
 }
 </style>
